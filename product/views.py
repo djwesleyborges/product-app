b054b95e8eff789c2a6f18c4a71464e9bdd83ebe
rg3915 <regis42santos@gmail.com>
2023-11-02 12:56:34 -0300

    Adiciona index.html

--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, template_name='base.html')
+    return render(request, template_name='index.html')
 
 
 def file(request):
