2ba4293418164bb8988aba540aade6d0d1f59ef2
rg3915 <regis42santos@gmail.com>
2023-11-02 12:52:04 -0300

    lint: aplica lint em product.js

--- static/js/product.js
+++ static/js/product.js
@@ -1,36 +1,32 @@
 const getItems = () => ({
-    filteredItems: [],
-    sizes: ['P', 'M', 'G', 'GG'],
-    currentImage: '',
+  filteredItems: [],
+  sizes: ['P', 'M', 'G', 'GG'],
+  currentImage: '',
 
-    init() {
-        this.getData()
-    },
+  init() {
+    this.getData()
+  },
 
-    getData() {
-        axios.get('/api/v1/product')
-            .then(response => {
-                //console.log(response.data[0].size)
-                this.filteredItems = response.data
-            })
-    },
+  getData() {
+    axios.get('/api/v1/product')
+      .then(response => {
+        this.filteredItems = response.data
+      })
+  },
 
-    getProduct(size, productId) {
-        //console.log(size);
-        //console.log(productId);
-        axios.get(`/api/v1/product/${productId}/image/${size.size}`)
-            .then(response => {
-                this.currentImage = response.data[0].image
-                //console.log(this.currentImage)
-                document.getElementById("imgChange").src = this.currentImage
-            })
-    },
+  getProduct(size, productId) {
+    axios.get(`/api/v1/product/${productId}/image/${size.size}`)
+      .then(response => {
+        this.currentImage = response.data[0].image
+        document.getElementById("imgChange").src = this.currentImage
+      })
+  },
 
-    getImageColor(color, productId) {
-        axios.get(`/api/v1/product/${productId}/color/${color.color}`)
-            .then(response => {
-                this.currentImage = response.data[0].image
-                document.getElementById("imgChange").src = this.currentImage
-            })
-    },
+  getImageColor(color, productId) {
+    axios.get(`/api/v1/product/${productId}/color/${color.color}`)
+      .then(response => {
+        this.currentImage = response.data[0].image
+        document.getElementById("imgChange").src = this.currentImage
+      })
+  },
 })
